bd53714d4972835d7b7ac0e775de0d6e89c63b44
slingshort <salina.myat@gmail.com>
2022-08-17 20:00:39 +1000

    modified js document, starting to build the function for "generatePassword", still needs to be commented and updated.

--- script.js
+++ script.js
@@ -1,9 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword {
+var numbers = '1234567890'
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUV'
+var lowerVase = 'abcdefghijklmnopqrstuv'
+var special = '!@#$%^&*()_+'
 
-}
+function generatePassword() {
+
+  var userChoice = window.prompt("How many characters would you like your password to contain?");
+
+  if (!userChoice) {
+    return;
+  } 
+
+
+  if (userChoice < 8 || userChoice > 128) {
+    console.log("invalid choice")
+    window.alert("Please choose a number between 8 and 128") 
+    return;
+  }
+
+  // var resultSpecial = 1;
+
+  if (window.confirm("Do you want your password to have special characters?")) {
+    console.log("true");
+  } else { console.log("false");}
+    
+
+  (window.confirm("Do you want your password to have upper case characters?"))
+    var resultUpper = "";
+  
+  (window.confirm("Do you want your password to have lower case characters?"))
+    var resultLower = "";
+  
+  (window.confirm("Do you want your password to have numerical characters?"))
+    var resultNumber = "";
+
+
+    
+  // for (let i = 0; i < userChoice; i++) {
+  //   result += special.charAt(Math.floor(Math.random() * special.length));
+      
+    // }
+
+
+
+  
+ 
+    
+   return;
+  }
+
+
+  
+
 
 // Write password to the #password input
 function writePassword() {
